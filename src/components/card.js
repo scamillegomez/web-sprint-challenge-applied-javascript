872882dbae1fd6318f3082e6714163a9fb2f0763
scgomez <sydneycamille0896@gmail.com>
2023-07-02 19:12:41 -0500

    completed components

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -17,6 +18,40 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+
+  // create HTML elements
+  let cardContainer = document.createElement('div');
+  let headlineDiv = document.createElement('div');
+  let authorDiv = document.createElement('div');
+  let imgDiv = document.createElement('div');
+  let image = document.createElement('img');
+  let bySpan = document.createElement('span');
+
+  // add classes
+  cardContainer.classList.add('card');
+  headlineDiv.classList.add('headline');
+  authorDiv.classList.add('author');
+  imgDiv.classList.add('img-container');
+
+  // add content
+  headlineDiv.textContent = article.headline;
+  authorDiv.textContent = article.authorName;
+  image.src = article.authorPhoto;
+  bySpan.textContent = `By ${article.authorName}`;
+
+  // create hierarchy
+  cardContainer.appendChild(headlineDiv);
+  cardContainer.appendChild(authorDiv);
+  authorDiv.appendChild(imgDiv);
+  authorDiv.appendChild(bySpan);
+  imgDiv.appendChild(image);
+
+  // add listener event
+  cardContainer.addEventListener('click',()=>{
+    console.log(article.headline);
+  })
+
+  return cardContainer;
 }
 
 const cardAppender = (selector) => {
@@ -28,6 +63,26 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+  axios.get(`http://localhost:5001/api/articles`)
+  .then(result=>{
+    // define parent element to append to
+    const articleEntryPoint = document.querySelector(selector);
+    // loop over topics
+    Object.entries(result.data.articles).forEach(([key,value])=>{
+      // loop over each "article" within the topics
+      value.forEach(el=>{
+        console.log(el);
+        const articleCard = Card(el);
+        articleEntryPoint.appendChild(articleCard);
+      })
+    })
+  })
+  .catch(err=>{
+    console.log(err);
+  })
+
 }
+
+cardAppender('.cards-container');
 
 export { Card, cardAppender }
